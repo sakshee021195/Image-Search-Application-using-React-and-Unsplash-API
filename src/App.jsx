3c58e13c6255e01e1f56438809570cb7554e4d7b
sakshee021195 <sakshee.jambucha@gmail.com>
2024-11-29 14:13:26 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Img from './Components/img'
+import Img from './Components/Img'
 
 function App() {
   const [count, setCount] = useState(0)
